6cfa78d69e366112ccf55cb8224eb184bc1017eb
DevilYT0778 <devilytontop@gmail.com>
2026-04-11 14:28:19 +0000

    Fix startup.sh

--- startup.sh
+++ startup.sh
@@ -11,10 +11,4 @@ cd /workspaces/HydraHosting/hydra
 nohup node . > /tmp/hydra.log 2>&1 &
 echo "Hydra started!"
 
-sleep 3
-
-cd /workspaces/HydraHosting/HydraPanel
-nohup node . > /tmp/hydrapanel.log 2>&1 &
-echo "HydraPanel started!"
-
 echo "=== All services running ==="
